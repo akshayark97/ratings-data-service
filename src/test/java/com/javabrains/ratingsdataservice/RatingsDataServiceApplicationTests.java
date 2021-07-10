2b4b8d3aac683fd0f8b2d2b6815782672767ff41
Akshay <akshayark97.ar@gmail.com>
2021-07-10 15:11:34 +0530

    first commit

---
+++ src/test/java/com/javabrains/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabrains.ratingsdataservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
